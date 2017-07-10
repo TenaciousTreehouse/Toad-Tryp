78dd2d0d77581ef300a5e57468125270fb906491
David Oh <davidjohis@gmail.com>
2017-07-10 15:34:36 -0700

    fixed merge conflicts

--- client/src/components/Search/SearchResultRow.jsx
+++ client/src/components/Search/SearchResultRow.jsx
@@ -4,7 +4,6 @@ import formatTime from '../utils/formatTime.js';
 import dateParser from '../utils/dateParser.js';
 
 const SearchResultRow = (props) => (
-  
   <Table.Body>
   <Table.Row style={{cursor:'pointer'}} className="results-row"onClick={() => props.handleClick(props.trip.id)}>
     <Table.Cell>
@@ -16,20 +15,7 @@ const SearchResultRow = (props) => (
     <Table.Cell singleLine>{props.driverDetails.year} {props.driverDetails.make || 'No Vehicle Information'} {props.driverDetails.model}</Table.Cell>
     <Table.Cell singleLine>{props.trip.seats}</Table.Cell>   
   </Table.Row>
-    <Table.Row style={{cursor:'pointer'}} className="results-row"onClick={() => props.handleClick(props.trip.id)}>
-      <Table.Cell>
-      <Header as='h3' textAlign='left' color='green'>${props.trip.price}</Header>
-      </Table.Cell>
-      <Table.Cell textAlign='left'> {props.trip.departure_city}, {props.trip.departure_state}</Table.Cell>
-      <Table.Cell singleLine>{props.trip.departure_time} <br/> {props.trip.departure_date}</Table.Cell>
-      <Table.Cell singleLine>{props.trip.arrival_city}, {props.trip.arrival_state}</Table.Cell>
-      <Table.Cell singleLine>{props.trip.arrival_time} <br/> {props.trip.arrival_date}</Table.Cell>  
-      <Table.Cell singleLine>{props.driverDetails.year} {props.driverDetails.make || 'No Vehicle Information'} {props.driverDetails.model}</Table.Cell>
-      <Table.Cell singleLine>{props.trip.seats}</Table.Cell>   
-    </Table.Row>
->>>>>>> fixed spacing in searchresults
   </Table.Body>
-  
 )
 
 export default SearchResultRow;
